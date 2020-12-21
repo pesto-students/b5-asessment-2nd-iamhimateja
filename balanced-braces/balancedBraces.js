1d5bbc191b3882e53c913f106c1348738438b73c
Himateja Merlapaka <iamhimateja@gmail.com>
2020-12-21 19:02:12 +0530

    B5 assessment Solutions for arrayCubeRootToJson, our own versions of array methods, balanced braces

--- balanced-braces/balancedBraces.js
+++ balanced-braces/balancedBraces.js
@@ -1,6 +1,16 @@
-
-function balancedBraces(...args) {
-  return args;
+function balancedBraces(input) {
+  const stack = [];
+  const bracePairs = { '{': '}', '[': ']', '(': ')' };
+  for (const charecter of input) {
+    if ('{[('.indexOf(charecter) !== -1) {
+      stack.push(charecter);
+    } else if ('}])'.indexOf(charecter) !== -1) {
+      if (bracePairs[stack[stack.length - 1]] === charecter) {
+        stack.pop();
+      }
+    }
+  }
+  return stack.length == 0;
 }
 
 export {
